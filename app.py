d8ae7fe31eeb8301ea884f5878ae51bd796b4127
jathusvarmanhacker-web <jathusvarmanhacker@gmail.com>
2026-05-17 18:08:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ from openai import OpenAI
 # =========================================================
 
 load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=os.getenv("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"))
 
 # =========================================================
 # PAGE CONFIG
